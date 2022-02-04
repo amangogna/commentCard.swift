081dd190fb6e8a17e798dddfd5b21c63d65a1a8b
Aman <gogna.a@etoncollege.org.uk>
2022-02-04 12:05:28 +0000

    Initial Commit

---
+++ commentCard.swift/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  commentCard.swift
+//
+//  Created by Gogna, Aman (HJRM) on 04/02/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ commentCard.swift/commentCard_swiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  commentCard_swiftApp.swift
+//  commentCard.swift
+//
+//  Created by Gogna, Aman (HJRM) on 04/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct commentCard_swiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
